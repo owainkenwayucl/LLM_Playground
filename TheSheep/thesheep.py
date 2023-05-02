8191e5188dd29b2d6a48ba7ab40b8202ce80994c
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-02 11:41:57 +0100

    More... usable.

--- TheSheep/thesheep.py
+++ TheSheep/thesheep.py
@@ -1,4 +1,4 @@
-def _main():
+def _main(memory="answer", debug=False):
 	from transformers import pipeline
 	import torch
 	import sys
@@ -14,23 +14,38 @@ def _main():
 			sys.exit()
 
 		instruction_text=""
-		if (len(instructions) > 0):
-			instruction_text = "Your previous responses were:\n"
-			i = 0
-			for a in instructions:
-				i = i + 1
-				instruction_text = instruction_text + str(i) + ". " + a + "\n"
-			instruction_text = instruction_text + "\nPlease answer the following user query: "
+		if (memory != "none"):
+			if (len(instructions) > 0):
+				instruction_text = "Your previous responses were:\n"
+				i = 0
+				for a in instructions:
+					i = i + 1
+					instruction_text = instruction_text + str(i) + ". " + a + "\n"
+				instruction_text = instruction_text + "\nPlease answer the following user query: "
 
-		instruction_text = instruction_text + line
+			instruction_text = instruction_text + line
+
+		if (debug):
+			print("------ PROMPT ------")
+			print(instruction_text)
+			print("------ PROMPT ------")
 
 		result = instruct_pipeline(instruction_text)
-		qr = "Question: " + line + "\nResponse:"
+
+		qr = ""
+		if (memory == "both"):
+			qr = "Question: " + line + "\nResponse:\n"
 		for a in result:
 			print("Sheep: " + a['generated_text'])
-			qr = qr + "\n" + a['generated_text']
+			qr = qr + a['generated_text']
 
 		instructions.append(qr)
 
 if __name__ == "__main__":
-	_main()
+	import argparse
+	parser = argparse.ArgumentParser(description="Simple Front-end for Dolly v2.0.")
+	parser.add_argument("-m", "--memory", metavar="memory", type=str, help="Memory style - allowed values are answer, none, both.", default="answer", choices=["none","answer","both"])
+	parser.add_argument("-d", action="store_true", help="Turn on Debug mode.", default=False)
+	args = parser.parse_args()
+	
+	_main(memory=args.memory, debug=args.debug)
