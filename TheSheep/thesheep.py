ac400b119d1ad1f489d0260c215761f80d76e37a
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-02 11:22:45 +0100

    Split main properly

--- TheSheep/thesheep.py
+++ TheSheep/thesheep.py
@@ -1,32 +1,36 @@
-from transformers import pipeline
-import torch
-import sys
+def _main():
+	from transformers import pipeline
+	import torch
+	import sys
 
-print("Setting up...")
-instruct_pipeline = pipeline(model="databricks/dolly-v2-12b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
+	print("Setting up...")
+	instruct_pipeline = pipeline(model="databricks/dolly-v2-12b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
 
-instructions = []
-print("The sheep is now ready.")
-while True:
-	line = input("? ")
-	if 'bye' == line.strip().lower():
-		sys.exit()
+	instructions = []
+	print("The sheep is now ready.")
+	while True:
+		line = input("? ")
+		if 'bye' == line.strip().lower():
+			sys.exit()
 
-	instruction_text=""
-	if (len(instructions) > 0):
-		instruction_text = "Your previous responses were:\n"
-		i = 0
-		for a in instructions:
-			i = i + 1
-			instruction_text = instruction_text + str(i) + ". " + a + "\n"
-		instruction_text = instruction_text + "\nPlease answer the following user query: "
+		instruction_text=""
+		if (len(instructions) > 0):
+			instruction_text = "Your previous responses were:\n"
+			i = 0
+			for a in instructions:
+				i = i + 1
+				instruction_text = instruction_text + str(i) + ". " + a + "\n"
+			instruction_text = instruction_text + "\nPlease answer the following user query: "
 
-	instruction_text = instruction_text + line
+		instruction_text = instruction_text + line
 
-	result = instruct_pipeline(instruction_text)
-	qr = "Question: " + line + "\nResponse:"
-	for a in result:
-		print("Sheep: " + a['generated_text'])
-		qr = qr + "\n" + a['generated_text']
+		result = instruct_pipeline(instruction_text)
+		qr = "Question: " + line + "\nResponse:"
+		for a in result:
+			print("Sheep: " + a['generated_text'])
+			qr = qr + "\n" + a['generated_text']
 
-	instructions.append(qr)
+		instructions.append(qr)
+
+if __name__ == "__main__":
+	_main()
