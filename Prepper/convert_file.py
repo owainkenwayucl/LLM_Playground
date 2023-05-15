f3da30c40723fd5d7a48fe5ea31c17cf366f3480
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-15 15:10:07 +0100

    Most basic reading of file
    Find data element and output it.

--- Prepper/convert_file.py
+++ Prepper/convert_file.py
@@ -1,4 +1,15 @@
 import messages
+import xml.etree.ElementTree
+
+_DATA_PATH="{http://www.tei-c.org/ns/1.0}text"
 
 def process(filename):
     messages.debug("Processing file: " + str(filename))
+
+    tree = ElementTree.parse(filename)
+    root = tree.getroot()
+    data = tree_root.findall(_DATA_PATH)[0]
+
+    output = ElementTree.tostring(data, encoding="utf-8", method="xml")
+    
+    messages.log(filename, output)
