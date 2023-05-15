7b760870ea09d8df096f1ddd1245195ead73224c
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-15 13:48:40 +0100

    Control flow implemented.

--- Prepper/prepper.py
+++ Prepper/prepper.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-
 # This script re-arranges data from the Transcribe Bentham dataset into a format that Dolly can use.
 
+import sys
+
+DEBUG=True
+
+def debug(message):
+    if (DEBUG):
+        print(">>> DEBUG <<< : " + str(message))
+
+def error(message):
+    sys.stderr.write(">>> ERROR <<< : " + str(message) + "\n")
+
 def _main(filename):
-    print("Processing " + str(filename))
+    debug("Processing file: " + str(filename))
 
 
 if __name__ == "__main__":
-    filename="test"
+    import argparse
+    parser = argparse.ArgumentParser(description="Data Convertor for Transcribe Bentham XML data")
+    parser.add_argument("-d", action="store_true", help="Turn on Debug mode.", default=False)
+    parser.add_argument("filename", metavar="filename", type=str, nargs="+", help="Files to process.")
+    args = parser.parse_args()
+	
+    DEBUG=args.d 
 
-    _main(filename)
+    for a in args.filename:
+        _main(a)
 
