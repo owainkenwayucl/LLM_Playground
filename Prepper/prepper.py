363b87564e697e760d044accb306bbc76882e109
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-15 13:34:31 +0100

    Start prepper.

---
+++ Prepper/prepper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# This script re-arranges data from the Transcribe Bentham dataset into a format that Dolly can use.
+
+def _main(filename):
+    print("Processing " + str(filename))
+
+
+if __name__ == "__main__":
+    filename="test"
+
+    _main(filename)
+
