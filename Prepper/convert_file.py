1a3a493c60bbb114d30f046337735cb1837da96d
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-15 15:22:16 +0100

    Add exception handling

--- Prepper/convert_file.py
+++ Prepper/convert_file.py
@@ -6,10 +6,15 @@ _DATA_PATH="{http://www.tei-c.org/ns/1.0}text"
 def process(filename):
     messages.debug("Processing file: " + str(filename))
 
-    tree = ElementTree.parse(filename)
-    tree_root = tree.getroot()
-    data = tree_root.findall(_DATA_PATH)[0]
+    try:
+        tree = ElementTree.parse(filename)
+        tree_root = tree.getroot()
+        data = tree_root.findall(_DATA_PATH)[0]
 
-    output = ElementTree.tostring(data, encoding="utf-8", method="xml")
+        output = ElementTree.tostring(data, encoding="utf-8", method="xml")
 
-    messages.log(output, filename)
+        messages.log(output, filename)
+
+    except Exception as e:
+        messages.error("XML Error in " + filename)
+        messages.error(str(e))
