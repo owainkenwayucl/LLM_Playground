afce914d4866761d9cf952b108fc8ce0e15fa5ce
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-16 11:44:15 +0100

    Fix directory type

--- Prepper/prepper.py
+++ Prepper/prepper.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Data Convertor for Transcribe Bentham XML data")
     parser.add_argument("-D", action="store_true", help="Turn on Debug mode.", default=False)
-    parser.add_argument("-o", metavar="outdir", help="Output folder for processed files.", default=".")
+    parser.add_argument("-o", metavar="outdir", tpye=str, help="Output folder for processed files.", default=".")
     parser.add_argument("filename", metavar="filename", type=str, nargs="+", help="Files to process.")
     args = parser.parse_args()
 	
