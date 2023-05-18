df9e91f00ad3d19091d10743e9094ee1408aa28f
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-18 13:31:08 +0100

    The second stage of the tutorial

---
+++ TheSheep/Tutorial/stagetwo.py
@@ -0,0 +1,19 @@
+def setup_pipeline():
+	import torch
+	from instruct_pipeline import InstructionTextGenerationPipeline
+	from transformers import AutoModelForCausalLM, AutoTokenizer
+
+	tokenizer = AutoTokenizer.from_pretrained("databricks/dolly-v2-12b", padding_side="left")
+	model = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-12b", device_map="auto", torch_dtype=torch.bfloat16)
+	pipeline = InstructionTextGenerationPipeline(model=model, task="text-generation", tokenizer=tokenizer, return_full_text=True)
+
+	return pipeline
+
+def setup_conversation(pipeline, debug=True):
+	from langchain import ConversationChain
+	from langchain.llms import HuggingFacePipeline
+
+	llm = HuggingFacePipeline(pipeline=pipeline)
+	conversation = ConversationChain(llm=llm, verbose=debug)
+	
+	return conversation
