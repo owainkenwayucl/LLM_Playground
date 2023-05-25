a001db73969022edda24c10a7c1d71e3e796c593
Owain Kenway <o.kenway@ucl.ac.uk>
2023-05-25 12:53:37 +0100

    Apparently we have to convert to HF format

--- TheDeer/build_vicuna.sh
+++ TheDeer/build_vicuna.sh
@@ -11,17 +11,24 @@ LLAMA_PATH=${LLAMA_PATH:-"${HOME}/Scratch/llama"}
 
 module load openssl/1.1.1t python/3.11.3
 
+rm -rf vicuna_runtime
 virtualenv vicuna_runtime
 source vicuna_runtime/bin/activate
 
 pip install fschat
 
+python3 -m transformers.models.llama.convert_llama_weights_to_hf \
+    --input_dir ${LLAMA_PATH} --model_size 7B --output_dir ${LLAMA_PATH}/7B_hf
+
+python3 -m transformers.models.llama.convert_llama_weights_to_hf \
+    --input_dir ${LLAMA_PATH} --model_size 13B --output_dir ${LLAMA_PATH}/13B_hf
+
 python3 -m fastchat.model.apply_delta \
-    --base-model-path ${LLAMA_PATH}/7B \
+    --base-model-path ${LLAMA_PATH}/7B_hf \
     --target-model-path vicuna-7b \
     --delta-path lmsys/vicuna-7b-delta-v1.1
 
 python3 -m fastchat.model.apply_delta \
-    --base-model-path ${LLAMA_PATH}/llama-13b \
+    --base-model-path ${LLAMA_PATH}/llama-13b_hf \
     --target-model-path vicuna-13b \
     --delta-path lmsys/vicuna-13b-delta-v1.1
