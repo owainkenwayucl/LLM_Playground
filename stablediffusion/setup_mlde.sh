3316aad4763b0c9bc1105011d8bf9069b485e353
Owain Kenway <o.kenway@ucl.ac.uk>
2023-09-26 13:46:47 +0100

    link rather than copy.

--- stablediffusion/setup_mlde.sh
+++ stablediffusion/setup_mlde.sh
@@ -2,5 +2,6 @@
 
 conda install --file requirements-conda.txt
 python -m ipykernel install --name=conjurer
-cp /data/autobentham/LLM_Playground/stablediffusion/sd_generator.py /run/determined/workdir
-cp /data/autobentham/LLM_Playground/stablediffusion/*.ipynb /run/determined/workdir
+ln -s /data/autobentham/LLM_Playground/stablediffusion/sd_generator.py /run/determined/workdir
+ln -s /data/autobentham/LLM_Playground/stablediffusion/Example\ Stable\ Diffusion.ipynb /run/determined/workdir
+ln -s /data/autobentham/LLM_Playground/stablediffusion/Image\ Viewer.ipynb /run/determined/workdir
