ef156f8de3c9b3d700191a7565a7a85006938bff
Owain Kenway <o.kenway@ucl.ac.uk>
2023-10-09 11:36:52 +0100

    Force GPU pytorch

--- stablediffusion/setup_mlde.sh
+++ stablediffusion/setup_mlde.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda install --file requirements-conda.txt
+conda install --file requirements-conda-mlde.txt
 python -m ipykernel install --name=conjurer
 ln -s /data/autobentham/LLM_Playground/stablediffusion/sd_generator.py /run/determined/workdir
 ln -s /data/autobentham/LLM_Playground/stablediffusion/Example\ Stable\ Diffusion.ipynb /run/determined/workdir
