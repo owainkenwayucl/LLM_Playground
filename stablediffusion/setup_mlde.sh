d1647af4addf3d7cb953e9e9c04bde239e474397
Owain Kenway <o.kenway@ucl.ac.uk>
2023-10-10 10:47:23 +0100

    Add link for parallel notebook.

--- stablediffusion/setup_mlde.sh
+++ stablediffusion/setup_mlde.sh
@@ -4,4 +4,5 @@ conda install --file requirements-conda-mlde.txt
 python -m ipykernel install --name=conjurer
 ln -s /data/autobentham/LLM_Playground/stablediffusion/sd_generator.py /run/determined/workdir
 ln -s /data/autobentham/LLM_Playground/stablediffusion/Example\ Stable\ Diffusion.ipynb /run/determined/workdir
+ln -s /data/autobentham/LLM_Playground/stablediffusion/Example\ Stable\ Diffusion\ Parallel.ipynb /run/determined/workdir
 ln -s /data/autobentham/LLM_Playground/stablediffusion/Image\ Viewer.ipynb /run/determined/workdir
