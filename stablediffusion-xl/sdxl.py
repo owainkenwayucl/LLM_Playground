c02b39d22422f9b71311623ef219e86344f5ce4d
Owain Kenway <o.kenway@ucl.ac.uk>
2023-10-22 21:22:44 +0100

    Attempt parallelisation
    Re-write of stable diffusion parallel.

--- stablediffusion-xl/sdxl.py
+++ stablediffusion-xl/sdxl.py
@@ -19,18 +19,74 @@ def setup_pipeline(model=model, model_r=model_r):
 
     return pipe,refiner
 
-def inference_denoise(pipe, refiner, prompt=default_prompt, pipe_steps=100, fname=default_fname, denoise=0.8, save=True):
-    image = pipe(prompt=prompt, num_inference_steps=pipe_steps, denoising_end=denoise).images[0]
-    image_r = refiner(prompt=prompt, image=image, num_inference_steps=pipe_steps, denoising_start=denoise).images[0]
-    if save:
-        image.save(f"{fname}.png")
-        image_r.save(f"{fname}_r.png")
+def inference_denoise(pipe, refiner, prompt=default_prompt, num_gen=1, pipe_steps=100, fname=default_fname, denoise=0.8, save=True, start=0):
+    images = []
+    images_r = []
+    for count in range(start, start+num_gen):
+        image = pipe(prompt=prompt, num_inference_steps=pipe_steps, denoising_end=denoise).images[0]
+        image_r = refiner(prompt=prompt, image=image, num_inference_steps=pipe_steps, denoising_start=denoise).images[0]
+        if save:
+            image.save(f"{fname}.png")
+            image_r.save(f"{fname}_r.png")
+        images.append(image)
+        images_r.append(image_r)
 
-    return image, image_r
+    return images, images_r
 
-def inference(pipe, prompt=default_prompt, pipe_steps=100, fname=default_fname, save=True):
-    image = pipe(prompt=prompt, num_inference_steps=pipe_steps).images[0]
-    if save:
-        image.save(f"{fname}.png")
+def inference(pipe, prompt=default_prompt, num_gen=1, pipe_steps=100, fname=default_fname, save=True, start=0):
+    images = []
+    for count in range(start, start+num_gen):
+        image = pipe(prompt=prompt, num_inference_steps=pipe_steps).images[0]
+        images.append[image]
+        if save:
+            image.save(f"{fname}_{count}.png")
 
-    return image
+    return images
+
+def _inference_worker(q, model=model, prompt=default_prompt, denoise=False, num_gen=1, pipe_steps=100, fname=default_fname, save=True, start=0):
+    pipe, pipe_r = setup_pipeline(model, model_r)
+    if denoise  = False:
+        images = inference(pipe=pipe, prompt=prompt, num_gen=num_gen, pipe_steps=pipe_steps, fname=fname, save=save, start=start)
+    else:
+        _,images = inference_denoise(pipe=pipe, refiner=pipe_r, prompt=prompt, num_gen=num_gen, pipe_steps=pipe_steps, fname=fname, denoise=denoise, save=save, start=start))
+    for a in images:
+        q.put(a)
+
+def parallel_inference(model=model, prompt=default_prompt, denoise=False, num_gen=1, pipe_steps=100, fname=default_fname, save=True):
+    from torch.multiprocessing import Process, Queue, set_start_method
+    import os
+
+    set_start_method("spawn")
+
+    number = platform["number"]
+
+    if num_gen < number:
+        print(f"Number of images to generate < number of GPUs, setting number of GPUs to {num_gen}")
+        number = num_gen
+    
+    # Decompose range into chunks
+    chunks = [ int(num_gen/number) ] * number
+    for a in range(num_gen - sum(chunks)):
+        chunks[a] +=1
+
+    starts = []
+    starts_rt = 0
+
+    for a in range(number):
+        starts.append(starts_rt)
+        starts_rt += chunks[a]
+
+    q = Queue()
+    procs = []
+
+    images = []
+
+    for a in range(number):
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(a)
+        procs.append(Process(target=_inference_worker, args=(q, model, prompt, denoise, chunks[a], iterations, save, starts[a])))
+        procs[a].start()
+
+    for a in range(num_gen):
+        images.append(q.get())
+    
+    return images
