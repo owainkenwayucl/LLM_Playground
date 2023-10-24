5f9f868e995c05823b5b364b7cf3e43fcde131c2
Owain Kenway <o.kenway@ucl.ac.uk>
2023-10-24 10:31:46 +0100

    Start CLI script

---
+++ stablediffusion-xl/sdxl_cli.py
@@ -0,0 +1,7 @@
+from sdxl import setup_pipeline, inference
+
+prompt = "A very happy bulb of garlic, oil paint"
+
+pipeline,_ = setup_pipeline(refiner_enabled = False)
+
+images = inference(pipeline=pipeline, prompt=prompt)
