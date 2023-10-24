a906b5a2199e2450acc0af19e2351a8ad6b2b7fb
Owain Kenway <o.kenway@ucl.ac.uk>
2023-10-24 11:19:17 +0100

    Add some options management

--- stablediffusion-xl/sdxl_cli.py
+++ stablediffusion-xl/sdxl_cli.py
@@ -1,7 +1,18 @@
-from sdxl import setup_pipeline, inference
+DEFAULT_NUM_GEN=1
+DEFAULT_ITERATIONS=50
+DEFAULT_FNAME="output"
 
-prompt = "A very happy bulb of garlic, oil paint"
+if __name__ == "__main__":
+    from sdxl import setup_pipeline, inference
+    prompt = "A very happy bulb of garlic, oil paint"
 
-pipeline,_ = setup_pipeline(refiner_enabled = False)
+    num_gen = int(ask("Number to generate", str(DEFAULT_NUM_GEN)))
+    
+    prompt = ask("Prompt", prompt)
+    fname = ask("File name", DEFAULT_FNAME)
 
-images = inference(pipe=pipeline, prompt=prompt)
+    iterations = int(ask("Inference iterations", str(DEFAULT_ITERATIONS)))
+
+    pipeline,_ = setup_pipeline(refiner_enabled = False)
+
+    images = inference(pipe=pipeline, prompt=prompt, fname=fname, num_gen=num_gen, pipe_steps=iterations)
