f37ad6832f127390b941c989932466e70c0493c6
Owain Kenway <o.kenway@ucl.ac.uk>
2023-10-30 15:17:04 +0000

    fix instll

--- stablediffusion/setup_inf1.sh
+++ stablediffusion/setup_inf1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python -m pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
-pip install ../requirements/inf1_neuron
+pip install -r ../requirements/inf1_neuron
