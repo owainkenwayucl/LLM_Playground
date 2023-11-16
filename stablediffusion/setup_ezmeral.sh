c8114ed897ee0253c38474f888d4e62c7772c97b
Owain Kenway <o.kenway@ucl.ac.uk>
2023-11-16 11:05:48 +0000

    Comment about CUDA_VISIBLE_DEVICES

--- stablediffusion/setup_ezmeral.sh
+++ stablediffusion/setup_ezmeral.sh
@@ -13,3 +13,5 @@ conda install python=3.11.5
 pip3 install --upgrade pip torch torchvision diffusers transformers accelerate safetensors ipykernel nvitop
 
 python3 -m ipykernel install --user --name=stablediffusion
+
+#export CUDA_VISIBLE_DEVICES=0,1
