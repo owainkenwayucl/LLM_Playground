8f34d2237362bfd4085e55790e4d13999ea37d81
Owain Kenway <o.kenway@ucl.ac.uk>
2023-11-29 17:11:22 +0000

    Increase heading level in output

--- llama2/General/lbctool.py
+++ llama2/General/lbctool.py
@@ -34,16 +34,16 @@ Projects are referred to as Epics.
 
 The format of a lean business case is as follows, with the bits you need to fill in between square braces []:
 
-# Epic Description:
+## Epic Description:
 [Describe the epic clearly and succinctly in 2-4 sentences]
 
-# Reason for Epic:
+## Reason for Epic:
 [Explain the reason for the epic. What are the current organisational or technical challenges, or opportunities for change? What problem(s) will be resolved and what outcomes are expected?] 
 
-# Is the epic needed to ensure UCL meets its statutory / regulatory requirements? [Yes/No]
+## Is the epic needed to ensure UCL meets its statutory / regulatory requirements? [Yes/No]
 [If yes, please provide details] 
 
-# Alignment to Strategic Themes and UCL Enablers
+## Alignment to Strategic Themes and UCL Enablers
 [Which of the following strategic themes does this epic contribute towards and how?]
 Digitise [Yes/No, if yes, describe how does this epic contribute to this theme?] 
 Modernise [Yes/No, if yes, describe how does this epic contribute to this theme?] 
@@ -52,41 +52,41 @@ Protect [Yes/No, if yes, describe how does this epic contribute to this theme?]
 
 [Does the epic contribute to any of UCL's Key Enablers, if so, please explain how] 
 
-# Outcomes for UCL:
+## Outcomes for UCL:
 [Describe the measurable benefits that UCL can expect to achieve]
 
-# Measures of Success:
+## Measures of Success:
 [Describe the measures that will be used to determine the success of the epic] 
 
 [Detail what will be measured and how?]
 
-# Scope (Organisational):
+## Scope (Organisational):
 [Consider the breadth of the change in the context of the wider organisation. Authors should think about specific departments, teams and user groups and also processes or sub-processes that will be in or out of scope.]
 
 [Data migration, cleansing, transformation, changes, consolidation] 
 
-# Scope (Technical):
+## Scope (Technical):
 [Detail the technical areas in scope e.g., integrations, underlying infrastructure etc.] 
 
-# Scope (Data):
+## Scope (Data):
 [Consider any data required, data affected, and data created - data governance, ownership, migration, cleansing, transformation, changes, consolidation, extraction and reporting]
 
 [Is personal or sensitive data involved?]
 
-# Non-Functional Requirements:
+## Non-Functional Requirements:
 [Include any significant criteria that can be used to judge the operation of a system e.g., security, availability, performance, capacity, scalability, resilience.]
 
 [Any special items e.g., performance required at a specific level that are different from the norm]
 
 [Does the outcome of this initiative have any kind of human interface? If it does it will require an accessibility review from the Digital Accessibility Team to ensure compliance with accessibility regulations. You can engage with the Digital Accessibility team for advice and support at digitalaccessibility@ucl.ac.uk] 
 
-# Minimum Viable Product Capabilities:
+## Minimum Viable Product Capabilities:
 [List of minimum viable product capabilities]
 
-# Additional Potential Capabilities:
+## Additional Potential Capabilities:
 [List of additional features the full product would have over the MVP]
 
-# Impact & Dependencies:
+## Impact & Dependencies:
 [Are there any dependencies or potential impact on other epics or features?]
 
 [Identify any other epics or features that may be impacted by this epic.]
@@ -95,7 +95,7 @@ Protect [Yes/No, if yes, describe how does this epic contribute to this theme?]
 
 [Describe any potential impact on the service offering in terms of how it is deployed and supported beyond the product team. Does this have a material impact on first- or second-line support and/or is there an impact on UCL - wide altering/monitoring systems?] 
 
-# Resource Impact:
+## Resource Impact:
 1. Can this epic be delivered and supported by existing product teams? [Yes/No]
 2. Do we need to add new capacity to an existing team and if so, how long for; just for implementation or for ongoing support? [Yes/No - if yes, estimate how long for and which role(s) are needed?] 
 3. Do we need to create a brand-new product team to deliver this epic? [Yes/No - if yes, estimate the size of the team] 
