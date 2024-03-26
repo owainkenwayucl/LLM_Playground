eab0884a4dc69bbcc0f3d457423905a455e57b0a
Owain Kenway <o.kenway@ucl.ac.uk>
2024-03-26 11:32:46 +0000

    Explicit trucnation

--- llama2/General/owain-llama.py
+++ llama2/General/owain-llama.py
@@ -51,7 +51,7 @@ while True:
     print(prompt)
     print("---")
 
-    output = llama_pipeline(prompt, do_sample=True, top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, max_length=len(prompt) + 200)[0]["generated_text"].split("[/INST]")[-1]
+    output = llama_pipeline(prompt, truncation=True, do_sample=True, top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, max_length=len(prompt) + 200)[0]["generated_text"].split("[/INST]")[-1]
     print(output)
 
     prompt = prompt + " " + output + " </s><s> [INST] "
