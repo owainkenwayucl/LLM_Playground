654ce702bab772063d3b0cae45245c8a9164f644
Owain Kenway <o.kenway@ucl.ac.uk>
2024-03-26 15:07:28 +0000

    Can't use args with gradient's weird wrappers/

--- llama2/GraphCore/lbctool.py
+++ llama2/GraphCore/lbctool.py
@@ -143,8 +143,10 @@ def cli_generate(line, checkpoint="7b", device_map="auto", oprint=True):
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) > 1:
-        with open(sys.argv[1], "r") as file:
+    promptfile="lbcprompt.txt"
+
+    if os.isfile(promptfile)
+        with open(promptfile, "r") as file:
             line = file.read()
             cli_generate(line)
     else: 
