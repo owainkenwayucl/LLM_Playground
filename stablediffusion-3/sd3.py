a19e44fa1557dd46ecdf7bf455f9ee4c8b1731cf
Owain Kenway <o.kenway@ucl.ac.uk>
2024-06-13 11:50:03 +0100

    Decrease log spam

--- stablediffusion-3/sd3.py
+++ stablediffusion-3/sd3.py
@@ -1,8 +1,11 @@
 # I'm going to do my best to make this super simple as the sdxl code is a mess!
 
 import torch
-from diffusers import StableDiffusion3Pipeline
+from diffusers import StableDiffusion3Pipeline, 
+from diffusers.utils import logging
 from utils import report_state, init_rng
+
+logging.set_verbosity_error() # Decrease somewhat unhinged log spam!
 
 model = "stabilityai/stable-diffusion-3-medium-diffusers"
 
