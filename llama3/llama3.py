490fc3770262bd88ea1e76fe04315d1fd7bba32c
Owain Kenway <o.kenway@ucl.ac.uk>
2024-06-26 10:49:15 +0100

    Llama 3 example

---
+++ llama3/llama3.py
@@ -0,0 +1,58 @@
+import transformers
+import torch
+
+size="8B"
+checkpoint_name = f"meta-llama/Meta-Llama-3-{size}-Instruct"  
+
+tokeniser - transformers.AutoTokenizer.form_pretrained(checkpoint_id)
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    checkpoint_name,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
+)
+
+messages_ = [
+    {"role": "system", "content": "You are a cute fluffy bear chatbot who always talks in uwu cute anime speak"},
+]
+
+messages = messages_
+
+while True:
+    line = input("? ")
+    if 'bye' == line.strip().lower():
+        sys.exit()
+
+    if 'forget' == line.strip().lower():
+        messages = messages_
+        continue
+
+    start = time.time()
+
+    line = line.strip()
+    messages.append({"role":"user","content":line})
+
+    input_ids = tokeniser.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        return_tensors="pt"
+    ).to(model.device)
+
+    terminators = [
+        tokeniser.eos_token_id,
+        tokeniser.convert_tokens_to_ids("<|eot_id|>")
+    ]
+
+    outputs = model.generate(
+        input_ids,
+        max_new_tokens=256,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9,
+    )
+    response = tokeniser.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokes=True)
+
+    print(response)
+
+    messages.append({"role":"assistant","content":response})
+
