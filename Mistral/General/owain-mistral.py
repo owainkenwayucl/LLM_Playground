de97688c1531fce97471ce3c4ca75291bb054f94
Owain Kenway <o.kenway@ucl.ac.uk>
2024-06-26 12:09:21 +0100

    Port in llama3 behaviour

--- Mistral/General/owain-mistral.py
+++ Mistral/General/owain-mistral.py
@@ -1,68 +1,77 @@
-import huggingface_hub
-from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
-import os
 import sys
-import time
 
-model_size = "7B"
-  
-checkpoint_name = f"mistralai/Mistral-{model_size}-Instruct-v0.2"
+import logging
 
-start = time.time()
-tokenizer = AutoTokenizer.from_pretrained(checkpoint_name)
+logging.disable(logging.WARNING)
 
-pipeline = AutoModelForCausalLM.from_pretrained(
-    pretrained_model_name_or_path=checkpoint_name,
-    torch_dtype=torch.float16,
+size="7B"
+checkpoint_name = f"mistralai/Mistral-{size}-Instruct-v0.2"
+
+tokeniser = transformers.AutoTokenizer.from_pretrained(checkpoint_name)
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    checkpoint_name,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
 )
 
-device="cuda"
+messages_ = [
+    {"role": "system", "content": "You are a cute fluffy bear chatbot who always talks in uwu cute anime speak"},
+]
 
-print(f"Model preparation time: {time.time() - start}s")
-
-_prompt = '''
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible.
-
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-
-
-'''
-
-__prompt = [{"role":"user","content": _prompt}]
-
-prompt = __prompt
-DEBUG = True
+messages = messages_
+avatar = "🧸"
 
 while True:
     line = input("? ")
     if 'bye' == line.strip().lower():
         sys.exit()
 
+    if 'reprogram' == line.strip().lower():
+        print(f"Current prompt: {messages_}")
+        print(f"Current avatar: {avatar}")
+
+        new_prompt = input("New prompt: ")
+        avatar = input("New avatar: ")
+
+        messages_ = [
+            {"role": "system", "content": new_prompt},
+        ]
+        messages = messages_
+        continue
+        
+
+
     if 'forget' == line.strip().lower():
-        prompt = __prompt
+        messages = messages_
         continue
 
-    start = time.time()
+    line = line.strip()
+    messages.append({"role":"user","content":line})
 
-    if len(prompt) == 1:
-        prompt = [{"role":"user","content": _prompt + line}]
-    else:
-        prompt.append({"role":"user","content": line})
+    input_ids = tokeniser.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        return_tensors="pt"
+    ).to(model.device)
 
-    encodes = tokenizer.apply_chat_template(prompt, return_tensors="pt")
+    terminators = [
+        tokeniser.eos_token_id,
+        tokeniser.convert_tokens_to_ids("<|eot_id|>")
+    ]
 
-    pipeline_inputs = encodes.to(device)
-    pipeline.to(device)
+    outputs = model.generate(
+        input_ids,
+        max_new_tokens=256,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9,
+    )
+    response = tokeniser.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
 
-    _output = pipeline.generate(pipeline_inputs, max_new_tokens=1000, do_sample=True)
+    print(f"{avatar} : {response}")
 
-    output = tokenizer.batch_decode(_output)[0]
+    messages.append({"role":"assistant","content":response})
 
-    prompt.append({"role":"assistant","content": output})
-
-    print(output)
-
-    elapsed = time.time() - start
-    print(" => Elapsed time: " + str(elapsed) + " seconds")
