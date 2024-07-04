46db9aee71b88d17d868179d2b9c58d5cd18df1f
Owain Kenway <o.kenway@ucl.ac.uk>
2024-07-04 16:45:49 +0100

    Granite for Habana

---
+++ granite/granite-habana.py
@@ -0,0 +1,87 @@
+import transformers
+import torch
+import sys
+
+import habana_frameworks.torch.core as htcore
+from optimum.habana.transformers.modeling_utils import adapt_transformers_to_gaudi
+
+adapt_transformers_to_gaudi()
+
+import logging
+
+logging.disable(logging.WARNING)
+
+size="7b"
+checkpoint_name = f"ibm-granite/granite-{size}-instruct"   
+
+tokeniser = transformers.AutoTokenizer.from_pretrained(checkpoint_name)
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    checkpoint_name,
+    torch_dtype=torch.bfloat16,
+).to(torch.device("hpu"))
+
+model = torch.compile(model,backend="hpu_backend")
+
+messages_ = [
+    {"role": "system", "content": "You are a helpful and efficient AI chatbot."},
+]
+
+messages = messages_
+avatar = "🧸"
+
+while True:
+    line = input("? ")
+    if 'bye' == line.strip().lower():
+        sys.exit()
+
+    if 'reprogram' == line.strip().lower():
+        print(f"Current prompt: {messages_}")
+        print(f"Current avatar: {avatar}")
+
+        new_prompt = input("New prompt: ")
+        avatar = input("New avatar: ")
+
+        messages_ = [
+            {"role": "system", "content": new_prompt},
+        ]
+        messages = messages_
+        continue
+        
+    if 'inspect' == line.strip().lower():
+        print(f"Model: {checkpoint_name}")
+        print(f"Chat state: {messages}")
+        print(f"Avatar: {avatar}")
+        continue
+
+    if 'forget' == line.strip().lower():
+        messages = messages_
+        continue
+
+    line = line.strip()
+    messages.append({"role":"user","content":line})
+
+    input_ids = tokeniser.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        return_tensors="pt"
+    ).to(model.device)
+
+    terminators = [
+        tokeniser.eos_token_id,
+        tokeniser.convert_tokens_to_ids("<|eot_id|>")
+    ]
+
+    outputs = model.generate(
+        input_ids,
+        max_new_tokens=512,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9,
+    )
+    response = tokeniser.decode(outputs[0][input_ids.shape[-1]:], skip_special_tokens=True)
+
+    print(f"{avatar} : {response}")
+
+    messages.append({"role":"assistant","content":response})
+
