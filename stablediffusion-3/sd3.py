c771a1562552065c21b7ef53214a38d441834472
Owain Kenway <o.kenway@ucl.ac.uk>
2024-07-28 15:59:56 +0100

    Apply fix for pytorch rocm issues
    https://github.com/ROCm/pytorch/issues/1500

--- stablediffusion-3/sd3.py
+++ stablediffusion-3/sd3.py
@@ -24,6 +24,18 @@ def detect_platform():
         r = nvidia
         r["number"] = torch.cuda.device_count()
         print(f" - {r['number']} GPUs detected")
+        # Fix for https://github.com/ROCm/pytorch/issues/1500
+        applied = False
+        for a in range(torch.cuda.device_count()):
+            device_name = torch.cuda.get_device_name(a)
+            if device_name.starts_with("AMD"):
+                applied = True
+                torch.backends.cuda.enable_mem_efficient_sdp(False)
+            print(f"Detected Cuda Device {a}: {device_name}")
+
+        if applied:
+            print(f"Disable memory efficient flash attention as fix for https://github.com/ROCm/pytorch/issues/1500")
+
     elif torch.backends.mps.is_available():
         print("Running on Apple GPU")
         r = metal 
