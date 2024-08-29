2b54eba36be59b4bd6e3278b8dc35a2295fa0824
Owain Kenway <o.kenway@ucl.ac.uk>
2024-08-29 13:58:33 +0100

    Side-port Flux memory hacks to SD3

--- stablediffusion-3/sd3.py
+++ stablediffusion-3/sd3.py
@@ -62,10 +62,21 @@ def setup_pipeline(model=model, exclude_t5=False, cpu_offload=False):
         #pipe.transformer.to("hpu") # not necessary it appears. 
         #pipe.vae.to("hpu")
 
-    pipe = pipe.to(platform["device"])
-
     if cpu_offload:
-        pipe.enable_module_cpu_offload()
+        print(f"Enabling sequential cpu offload. This will massively decrease memory usage but may break device selection.")
+        pipe.enable_sequential_cpu_offload()
+    else:
+        try:
+            pipe.enable_vae_tiling()
+        except:
+            print(f"This version of diffusers doesn't support VAE tiling yet.")
+            try:
+                pipe.vae.enable_slicing()
+                pipe.vae.enable_tiling()
+                print(f"Directly enabled VAE slicing + tiling")
+            except:
+                print(f"Can't directly enable VAE slicing + tiling either")
+        pipe = pipe.to(platform["device"])
 
     return pipe
 
