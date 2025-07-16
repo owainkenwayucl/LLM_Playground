139a247b6cc48e86ca30f58dc14d0803d64edf8b
Owain Kenway <o.kenway@ucl.ac.uk>
2025-07-16 10:43:10 +0100

    KServe files.

---
+++ modelserving/kubeflow/test_granite.py
@@ -0,0 +1,21 @@
+import requests
+
+base_model_endpoint = "" # Replace with your endpoint
+api_endpoint = f"{base_model_endpoint}/openai/v1/completions"
+headers = {
+    "Content-Type": "application/json",
+}
+
+payload = {
+    "model": "granite",
+    "prompt": "Tell me some facts about frogs.",
+    "max_tokens": 500
+}
+
+response = requests.post(api_endpoint, headers=headers, json=payload)
+
+if response.status_code == 200:
+    output = response.json()
+    print("Generated text:", output["choices"][0]["text"])
+else:
+    print("Request failed:", response.status_code, response.text)
