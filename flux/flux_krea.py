6852c614145ff687a9c62af8b9718b15a8241f69
Owain Kenway <o.kenway@ucl.ac.uk>
2025-08-28 14:22:37 +0100

    missing =

--- flux/flux_krea.py
+++ flux/flux_krea.py
@@ -138,7 +138,7 @@ def parallel_interactive_inference(prompt="", negative_prompt="",num_gen=1, num_
         procs.append(Process(target=inference_worker, args=(q, prompt, "", chunks[a], num_iters, 3.5, False, seed, image_width, image_height)))
         procs[a].start()
 
-    images []
+    images = []
     for a in range(num_gen):
         images.append(q.get())
 
