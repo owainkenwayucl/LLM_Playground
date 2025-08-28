c7b0e5c7ec9884a8c4286ac51dabec7f2f3682bb
Owain Kenway <o.kenway@ucl.ac.uk>
2025-08-28 14:21:34 +0100

    Positional arguments

--- flux/flux_krea.py
+++ flux/flux_krea.py
@@ -102,7 +102,7 @@ def interactive_inference(prompt="", negative_prompt="",num_gen=1, num_iters=50,
     torch.cuda.empty_cache()
 
 def inference_worker(queue, pipeline=None, prompt="", negative_prompt="", num_gen=1, num_iters=50, guidance_scale=3.5, seed=None, width=1024, height=1024, device_id=0):
-    pipeline = setup_pipeline(cpu_offload=cpu_offload, device_id)
+    pipeline = setup_pipeline(cpu_offload=cpu_offload, device_id=device_id)
     images = inference(pipeline=pipeline,prompt=prompt, negative_prompt=negative_prompt, num_gen=num_gen, num_iters=num_iters, guidance_scale=guidance_scale, seed=seed, width=width, height=height)
     del pipeline
     gc.collect()
