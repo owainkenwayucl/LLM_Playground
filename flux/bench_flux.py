2bcd28b8e66b898e6b80c1419578df4c0717e412
Owain Kenway <o.kenway@ucl.ac.uk>
2025-09-09 15:02:39 +0100

    do multiple images because of startup cost

--- flux/bench_flux.py
+++ flux/bench_flux.py
@@ -1,10 +1,10 @@
 from flux_krea import inference
 
 prompt="A frog sitting on a lily pond in an idylic swamp."
-
+num_gen=9
 width=1920
 height=1600
 num_steps=100
 seed=12345
 
-image = inference(prompt=prompt, num_iters=num_steps, width=width, height=height, seed=seed)
+image = inference(prompt=prompt, num_gen=9, num_iters=num_steps, width=width, height=height, seed=seed)
