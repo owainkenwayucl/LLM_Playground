bfd7b883afa1cd0e74481f5bcc0b3127404d7985
Owain Kenway <o.kenway@ucl.ac.uk>
2025-11-28 13:09:55 +0000

    Missing the tea

--- flux.2/flux.py
+++ flux.2/flux.py
@@ -78,7 +78,7 @@ def inference(pipeline=None, prompt="", negative_prompt="", num_gen=1, num_iters
         t_s = time.time()
         temp_s = generator.get_state()
         report_state(temp_s)
-        images.append(pipeline(promp=prompt, generator=generator, num_inference_steps=num_iters, guidance_scale=guidance_scale, width=width, height=height).images[0])
+        images.append(pipeline(prompt=prompt, generator=generator, num_inference_steps=num_iters, guidance_scale=guidance_scale, width=width, height=height).images[0])
         t_f = time.time()
         times.append(t_f - t_s)
 
