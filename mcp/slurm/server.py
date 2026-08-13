c0c85d03275bedf202425df87df2583621100ad1
Owain Kenway <o.kenway@ucl.ac.uk>
2026-08-13 15:04:32 +0100

    First attempt at an mcp server

---
+++ mcp/slurm/server.py
@@ -0,0 +1,45 @@
+from mcp.server import MCPServer
+import asyncio
+
+def run(command):
+	import subprocess
+	return subprocess.run(command, capture_output=True, encoding='UTF-8')
+
+mcp = MCPServer("Slurm")
+
+@mcp.resource("clusterinfo://")
+def cluster_info() -> str:
+	command = ["sinfo", "--json"]
+	return run(command).stdout
+
+@mcp.resource("clusterqueue://")
+def clusterqueue() -> str:
+	command = ["squeue", "--json"]
+	return run(command).stdout
+
+@mcp.tool()
+def submitjob(jobscript: str) -> int:
+	command = ["sbatch", jobscript]
+	return int(run(command.stdout))
+
+@mcp.tool()
+def canceljob(jobid: int):
+	command = ["scancel", str(jobid)]
+	run(command)
+
+@mcp.tool()
+def jobstatus(jobid: int) -> str:
+	command = ["squeue", "--json", "-j", jobid]
+	return run(command).stdout
+
+@mcp.tool()
+def clusterrun(command: str) -> int:
+	command_s = command.split()
+	command_a = ["srun"]
+	for a in command_s:
+		command_a.append(a)
+	return int(run(command_a).stdout
+
+# Run through stdio
+if __name__ == "__main__":
+	asyncio.run(mcp.run(transport="stdio"))
