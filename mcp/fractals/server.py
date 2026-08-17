04e55053b433ea6e58b72793b1bf355843aa9341
Owain Kenway <o.kenway@ucl.ac.uk>
2026-08-17 14:20:24 +0100

    Go back to returning lists

--- mcp/fractals/server.py
+++ mcp/fractals/server.py
@@ -1,35 +1,34 @@
 from mcp.server import MCPServer
 import asyncio
 import fractals
-from mcp.types import ImageContent, TextContent, CallToolResults, Annotations
+from mcp.types import ImageContent, TextContent
 
 mcp = MCPServer("Fractals")
 
 @mcp.tool(structured_output=False)
-def mandelbrot(width: int, height: int, xmin: float, xmax: float, ymin: float, ymax: float, max_iter: int) -> CallToolResults
+def mandelbrot(width: int, height: int, xmin: float, xmax: float, ymin: float, ymax: float, max_iter: int) -> list[ImageContent | TextContent]
 	image = fractals.write_image_base64(fractals.generate_fractal(width, height, fractals.mandel, xmin, xmax, ymin, ymax, max_iter))
 	
-	return CallToolResults(content=[
-		TextContent(type="text", text=f"Generated a Mandelbrot set: width: {width}, height: {height}, xmin: {xmin}, xmax: {xmax}, ymin: {ymin}, ymax: {ymax}, max_iter: {max_iter}"), 
+	return [
 		ImageContent(
 			type="image",
 			data=image,
 			mimeType="image/png",
-			annotations=Annotations(audience=["user", "assistant"]),
 		)
-	]), 
+		TextContent(type="text", text=f"Generated a Mandelbrot set: width: {width}, height: {height}, xmin: {xmin}, xmax: {xmax}, ymin: {ymin}, ymax: {ymax}, max_iter: {max_iter}"), 
+	]
 
 @mcp.tool(structured_output=False)
-def julia(width: int, height: int, xmin: float, xmax: float, ymin: float, ymax: float, max_iter: int, c: float, n: int) -> list[TextContent | ImageContent]:
+def julia(width: int, height: int, xmin: float, xmax: float, ymin: float, ymax: float, max_iter: int, c: float, n: int) -> list[ImageContent | TextContent]:
 	j = fractals.generate_julia(c,n)
 	image = fractals.write_image_base64(fractals.generate_fractal(width, height, j, xmin, xmax, ymin, ymax, max_iter))
 	
 	return [
-		TextContent(type="text", text=f"Generated a Julia set: width: {width}, height: {height}, xmin: {xmin}, xmax: {xmax}, ymin: {ymin}, ymax: {ymax}, max_iter: {max_iter}, c: {c}, n: {n}"), 
 		ImageContent(
 			type="image",
 			data=image,
 			mimeType="image/png"
+		TextContent(type="text", text=f"Generated a Julia set: width: {width}, height: {height}, xmin: {xmin}, xmax: {xmax}, ymin: {ymin}, ymax: {ymax}, max_iter: {max_iter}, c: {c}, n: {n}"), 
 		)
 	]
 
